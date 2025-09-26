f978f7c98cfa80f51418fb5d50ca439ee7ae52c7
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2025-09-26 18:48:37 +0300

    task 7: add Dimens.kt

---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Dimens.kt
@@ -0,0 +1,39 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val FontSize32 = 32.sp
+val FontSize22 = 22.sp
+val FontSize16 = 16.sp
+val FontSize12 = 12.sp
+
+val Padding46 = 46.dp
+val Padding32 = 32.dp
+val Padding28 = 28.dp
+val Padding24 = 24.dp
+val Padding22 = 22.dp
+val Padding20 = 20.dp
+val Padding18 = 18.dp
+val Padding16 = 16.dp
+val Padding12 = 12.dp
+val Padding8 = 8.dp
+val Padding4 = 4.dp
+
+val ComponentSize194 = 194.dp
+val ComponentSize108 = 108.dp
+val ComponentSize80 = 80.dp
+val ComponentSize64 = 64.dp
+val ComponentSize60 = 60.dp
+val ComponentSize56 = 56.dp
+val ComponentSize48 = 48.dp
+val ComponentSize36 = 36.dp
+val ComponentSize28 = 28.dp
+val ComponentSize24 = 24.dp
+val ComponentSize22 = 22.dp
+val ComponentSize20 = 20.dp
+val ComponentSize2 = 2.dp
+
+val CornerRadius15 = 15.dp
+val CornerRadius12 = 12.dp
+
--- app/src/main/java/ru/practicum/android/diploma/ui/theme/FontProvider.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/FontProvider.kt
@@ -1,4 +1,13 @@
 package ru.practicum.android.diploma.ui.theme
 
-class FontProvider {
-}
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import ru.practicum.android.diploma.R
+
+val ysDisplayFontFamily = FontFamily(
+    Font(R.font.ys_display_bold, FontWeight.Bold, FontStyle.Normal),
+    Font(R.font.ys_display_medium, FontWeight.Medium, FontStyle.Normal),
+    Font(R.font.ys_display_regular, FontWeight.Normal, FontStyle.Normal),
+)
