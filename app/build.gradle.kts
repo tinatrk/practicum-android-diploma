c3cfa29bab1a83b5c40105e0067224cc8f49ac65
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2025-09-26 17:14:19 +0300

    task-7: add base Colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,4 +55,7 @@ dependencies {
     androidTestImplementation(libs.uiTests.junitExt)
     androidTestImplementation(libs.uiTests.espressoCore)
     // endregion
+
+    // compose
+    implementation(libs.ui)
 }
---
+++ app/src/main/java/ru/practicum/android/diploma/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package ru.practicum.android.diploma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFDFDFD)
+val Black = Color(0xFF1A1B22)
+val Black50Alpha = Color(0x801A1B22)
+val Blue = Color(0xFF3772E7)
+val Red = Color(0xFFF56B6C)
+val Gray = Color(0xFFAEAFB4)
+val LightGray = Color(0xFFE6E8EB)
