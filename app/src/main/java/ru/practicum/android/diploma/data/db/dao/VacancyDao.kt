b152be020a6dded4e8c5b301b0b104d8d36d80ea
Ivan Drobitko <drobitkoivan@yandex.ru>
2025-09-28 19:47:31 +0500

    Исправил оишбки со стилем кода, а также добавил dto VacancyPreview

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/VacancyDao.kt
@@ -7,6 +7,7 @@ import androidx.room.OnConflictStrategy.Companion.REPLACE
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.db.entity.VacancyEntity
+import ru.practicum.android.diploma.data.db.dto.VacancyPreview
 
 @Dao
 interface VacancyDao {
@@ -20,6 +21,20 @@ interface VacancyDao {
     @Query("SELECT * FROM vacancy WHERE vacancyId = :vacancyId")
     fun getVacancyById(vacancyId: String): Flow<VacancyEntity?>
 
-    @Query("SELECT vacancyId, name, address_city, employer_name, employer_logo, salary_from, salary_to, salary_currency FROM vacancy ORDER BY addedTime DESC")
-    fun getVacanciesPreview(): Flow<List<VacancyEntity>?>
+    @Query(
+        """
+            SELECT 
+                vacancyId, 
+                name, 
+                address_city AS city, 
+                employer_name AS employerName, 
+                employer_logo AS employerLogo, 
+                salary_from AS salaryFrom, 
+                salary_to AS salaryTo, 
+                salary_currency AS salaryCurrency 
+                FROM vacancy 
+                ORDER BY addedTime DESC
+                """
+    )
+    fun getVacanciesPreview(): Flow<List<VacancyPreview>?>
 }
