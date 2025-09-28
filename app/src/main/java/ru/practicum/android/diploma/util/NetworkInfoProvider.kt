1bc098a73fb1f454c02411dd87bdad1a61240770
Tatyana Soloveva android_plus 24 <tatjana.solov2014@yandex.ru>
2025-09-28 14:46:33 +0300

    task 5: add utility funs (debounce, network connection info)

---
+++ app/src/main/java/ru/practicum/android/diploma/util/NetworkInfoProvider.kt
@@ -0,0 +1,31 @@
+package ru.practicum.android.diploma.util
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+
+class NetworkInfoProvider(
+    private val application: Application
+) {
+
+    fun isConnected(): Boolean {
+        val connectivityManager = application.getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        val capabilities = connectivityManager.getNetworkCapabilities(
+            connectivityManager.activeNetwork
+        )
+
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
+            }
+        }
+
+        return false
+    }
+}
