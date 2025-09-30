48c8fc0a243cfba37f7490b6a2fcd0d8c25395ab
Ivan Drobitko <drobitkoivan@yandex.ru>
2025-09-30 23:36:08 +0500

    Corrected code style

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -3,5 +3,5 @@ package ru.practicum.android.diploma.di
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    //TODO
+    // ...
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -13,7 +13,7 @@ class RootActivity : AppCompatActivity() {
         setContent {
             AppTheme {
                 KoinAndroidContext {
-                    //TODO
+                    // ...
                 }
             }
         }
