a81f286f638897012da7689615231ceb284061c8
Ivan Drobitko <drobitkoivan@yandex.ru>
2025-09-30 22:46:43 +0500

    Corrected libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("ru.practicum.android.diploma.plugins.developproperties")
+    id("kotlin-parcelize")
+    alias(libs.plugins.androidx.room)
     alias(libs.plugins.devtoolsKsp)
 }
 
@@ -21,6 +23,10 @@ android {
         buildConfigField(type = "String", name = "API_ACCESS_TOKEN", value = "\"${developProperties.apiAccessToken}\"")
     }
 
+    room {
+        schemaDirectory("$projectDir/schemas")
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -37,6 +43,11 @@ android {
 
     buildFeatures {
         buildConfig = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
@@ -56,9 +67,51 @@ dependencies {
     androidTestImplementation(libs.uiTests.junitExt)
     androidTestImplementation(libs.uiTests.espressoCore)
     // endregion
+
+    // Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    implementation(libs.material3)
+    implementation(libs.ui)
+    implementation(libs.ui.tooling.preview)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.lifecycle.viewmodel.compose)
+    implementation(libs.activity.compose)
+
+    // Navigation Compose
+    implementation(libs.navigation.compose)
+
+    // Koin
     implementation(libs.koin.androidx.compose)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter)
+
+    // Coil
+    implementation(libs.coil.compose)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    implementation(libs.kotlinx.collections.immutable)
+
+    // region Gson
+    implementation(libs.gson)
+    // endregion
+
+    // region compose
+    implementation(libs.ui)
+    implementation(libs.material3)
+    debugImplementation(libs.ui.tooling)
+    implementation(libs.runtime)
+    implementation(libs.navigation.compose)
+    // endregion
+
+    // region Room
     implementation(libs.room.runtime)
     ksp(libs.room.compiler)
     annotationProcessor(libs.room.compiler)
     implementation(libs.room.ktx)
+    // endregion
 }
