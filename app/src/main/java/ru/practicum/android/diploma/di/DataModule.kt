fe192f57af9ded37a4f8cf105d3577b86f19197b
Ivan Drobitko <drobitkoivan@yandex.ru>
2025-09-30 23:27:40 +0500

    Настроил DI и создал основные модули

---
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -0,0 +1,48 @@
+package ru.practicum.android.diploma.di
+
+import androidx.room.Room
+import com.google.gson.Gson
+import okhttp3.OkHttpClient
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.db.AppDatabase
+import ru.practicum.android.diploma.data.network.AuthInterceptor
+import ru.practicum.android.diploma.data.network.DiplomaApi
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+
+val dataModule = module {
+
+    single {
+        AuthInterceptor()
+    }
+
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(get<AuthInterceptor>())
+            .build()
+    }
+
+    single<DiplomaApi> {
+        Retrofit.Builder()
+            .baseUrl("https://practicum-diploma-8bc38133faba.herokuapp.com/")
+            .client(get<OkHttpClient>())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(DiplomaApi::class.java)
+    }
+
+    single<NetworkClient> {
+        RetrofitNetworkClient(get())
+    }
+
+    factory {
+        Gson()
+    }
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database")
+    }
+}
