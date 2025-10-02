2baea0c1089f03044a37cde21e162c79a9fcc929
Dmitriy <hoddlep@yandex.ru>
2025-10-02 15:35:02 +0500

    Исправлено форматирование

--- app/src/main/java/ru/practicum/android/diploma/domain/search/impl/VacancySearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/impl/VacancySearchInteractorImpl.kt
@@ -1,12 +1,12 @@
 package ru.practicum.android.diploma.domain.search.impl
 
+import kotlinx.coroutines.flow.Flow
 
+import ru.practicum.android.diploma.domain.search.models.VacancyPage
 import ru.practicum.android.diploma.domain.search.repository.VacancySearchInteractor
 import ru.practicum.android.diploma.domain.search.repository.VacancySearchRepository
 import ru.practicum.android.diploma.util.common.Failure
 import ru.practicum.android.diploma.util.common.Resource
-import ru.practicum.android.diploma.domain.search.models.VacancyPage
-import kotlinx.coroutines.flow.Flow
 
 class VacancySearchInteractorImpl(
     private val repository: VacancySearchRepository,
