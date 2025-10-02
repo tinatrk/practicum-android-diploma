fd2d5f56e571cabb5bc6e74c5d446a0213cee92d
Dmitriy <hoddlep@yandex.ru>
2025-10-02 14:50:57 +0500

    Добавлены три модели для представления результатов поиска вакансии

---
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/models/Salary.kt
@@ -0,0 +1,7 @@
+package ru.practicum.android.diploma.domain.search.models
+
+data class Salary(
+    val from: Int?,
+    val to: Int?,
+    val currency: String?
+)
---
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/models/Vacancy.kt
@@ -0,0 +1,8 @@
+package ru.practicum.android.diploma.domain.search.models
+
+data class Vacancy(
+    val id: String,
+    val name: String?,
+    val salary: Salary?,
+    val industry: String?
+)
