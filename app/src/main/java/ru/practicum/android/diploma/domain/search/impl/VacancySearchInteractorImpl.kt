373d8fc9ececde0e1c4a7a01c9d130861826ba09
Dmitriy <hoddlep@yandex.ru>
2025-10-02 16:30:18 +0500

    Изменен порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/domain/search/impl/VacancySearchInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/search/impl/VacancySearchInteractorImpl.kt
@@ -1,9 +1,9 @@
 package ru.practicum.android.diploma.domain.search.impl
 
 import kotlinx.coroutines.flow.Flow
-import ru.practicum.android.diploma.domain.search.models.VacancyPage
 import ru.practicum.android.diploma.domain.search.api.interactor.VacancySearchInteractor
 import ru.practicum.android.diploma.domain.search.api.repository.VacancySearchRepository
+import ru.practicum.android.diploma.domain.search.models.VacancyPage
 import ru.practicum.android.diploma.util.common.Failure
 import ru.practicum.android.diploma.util.common.Resource
 
