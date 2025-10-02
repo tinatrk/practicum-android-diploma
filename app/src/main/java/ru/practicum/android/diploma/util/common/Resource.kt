635071d4d21e107b9cb0fb19e036553d82d6d97f
Dmitriy <hoddlep@yandex.ru>
2025-10-02 15:27:35 +0500

    Исправлено форматирование

--- app/src/main/java/ru/practicum/android/diploma/util/common/Resource.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/common/Resource.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.util.common
 
 sealed class Resource<out T, out E> {
-    data class Success<T>(val data: T): Resource<T, Nothing>()
-    data class Error<E>(val error: E): Resource<Nothing, E>()
+    data class Success<T>(val data: T) : Resource<T, Nothing>()
+    data class Error<E>(val error: E) : Resource<Nothing, E>()
 }
